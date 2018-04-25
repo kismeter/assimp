ac3ce967f55e954e1fc9a03754b903421fd5c042
nola.donato@samsung.com <nola.donato@samsung.com>
2018-04-24 17:07:10 -0700

    updated build scripts

--- android_debug_build32.sh
+++ android_debug_build32.sh
@@ -1,3 +1,12 @@
-cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DANDROID_ABI=armeabi-v7a -DANDROID_NATIVE_API_LEVEL=android-24 -DANDROID_FORCE_ARM_BUILD=TRUE -DCMAKE_INSTALL_PREFIX=install -DANDROID_STL=gnustl_static -DANDROID_STL_FORCE_FEATURES=ON -DCMAKE_TOOLCHAIN_FILE=/home/nola.donato/android-sdks/ndk-bundle/build/cmake/android.toolchain.cmake -DANDROID_TOOLCHAIN=gcc
-make
+cmake	-G "Unix Makefiles" \
+	-DCMAKE_BUILD_TYPE=Debug  \
+	-DANDROID_ABI=armeabi-v7a \
+	-DANDROID_NATIVE_API_LEVEL=android-24 \
+	-DANDROID_FORCE_ARM_BUILD=TRUE \
+	-DCMAKE_INSTALL_PREFIX=install \
+	-DANDROID_STL=gnustl_static \
+	-DASSIMP_BUILD_TESTS=OFF \
+	-DANDROID_STL_FORCE_FEATURES=ON \
+	-DANDROID_TOOLCHAIN=gcc
+cmake --build .
 
