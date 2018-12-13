b2291f77412c33397f78947edbce5b49b7044307
Nola Donato <nola.donato@samsung.com>
2018-12-13 12:31:04 -0800

    fix intel build script

--- intel_release_build32.sh
+++ intel_release_build32.sh
@@ -9,8 +9,9 @@ $ANDROID_SDK/cmake/3.6.4111459/bin/cmake --trace -G "Unix Makefiles" \
 	-DCMAKE_INSTALL_PREFIX=install \
 	-DANDROID_STL=c++_shared \
 	-DANDROID_STL_FORCE_FEATURES=ON \
+	-DANDROID_TOOLCHAIN_NAME=x86-4.9 \
 	-DCMAKE_TOOLCHAIN_FILE=$NDKROOT/build/cmake/android.toolchain.cmake \
 	-DANDROID_TOOLCHAIN=clang
 $NDKROOT/prebuilt/linux-x86_64/bin/make -j12
-$NDKROOT/toolchains/x86-4.9/prebuilt/linux-x86_64/arm-linux-androideabi/bin/strip lib/libassimp.so
+$NDKROOT/toolchains/x86-4.9/prebuilt/linux-x86_64/i686-linux-android/bin/strip lib/libassimp.so
 
